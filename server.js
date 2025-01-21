3ffd301cb30110c34e1bf1ee197876e7026e2469
filipslunsky <fslunsky@gmail.com>
2025-01-21 03:02:17 +0100

    invoice JSON server set up

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`listening to port ${PORT}`);
+});
+
+app.use(cors());
+
+app.get('/invoices', (req, res) => {
+    const invoices = require('./config/data/data.json');
+    res.json(invoices);
+});
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
+});
