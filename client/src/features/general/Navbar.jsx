4564a344816b4fee6e32d8e3455568e712d635a9
filipslunsky <fslunsky@gmail.com>
2025-01-22 18:00:45 +0100

    navbar elements and functions added

--- client/src/features/general/Navbar.jsx
+++ client/src/features/general/Navbar.jsx
@@ -1,14 +1,30 @@
 import { useSelector, useDispatch } from "react-redux";
 import { toggleNightMode } from "./state/slice";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     
     const nightMode = useSelector(state => state.visual.nightMode);
 
+    const handleNightModeClick = () => {
+        dispatch(toggleNightMode());
+    };
+
+    const handleHomeClick = () => {
+        navigate('/');
+    };
+
     return (
-        <>
-            <h2>Navbar</h2>
+        <>  <div className="navbarMainContainer">
+                <h2>Navbar</h2>
+                <button className="homeButton" onClick={handleHomeClick}>home</button>
+                <button className="toggleNightButton" onClick={handleNightModeClick}>{nightMode ? 'ON' : 'OFF'}</button>
+                <div className="userImageDiv">
+                    <img src="" alt="user image" />
+                </div>
+            </div>
         </>
     );
 }
