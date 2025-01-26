f5e2b3ffb86e445b622d37294162cccc09a5634d
filipslunsky <fslunsky@gmail.com>
2025-01-27 00:57:59 +0100

    added globalContainer to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import InvoiceList from './features/invoices/InvoiceList';
 import Navbar from './features/general/Navbar';
 import InvoiceDetail from './features/invoices/InvoiceDetail';
 import './App.css';
 
 function App() {
+  const nightMode = useSelector(state => state.visual.nightMode);
 
   return (
     <>
+      <div className={nightMode ? "globalContainer nightMode" : "globalContainer"}>
       <BrowserRouter>
         <Navbar />
         <Routes>
@@ -16,6 +19,7 @@ function App() {
           <Route path='/' element={<InvoiceList />} />
         </Routes>
       </BrowserRouter>
+      </div>
     </>
   )
 }
